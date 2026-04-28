0570b617bbfe7f87055832c67ba1b76d77960516
Nilesh Parmar <nilesh2033parmar@gmail.com>
2026-04-28 15:36:32 +0530

    New Docker Project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>🚀 React + Docker App</h1>
+      <p>DevOps Practice Project</p>
+
+      <div className="card">
+        <h2>Counter: {count}</h2>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
